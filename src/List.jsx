0882ffd7e42c7206c877b36f4d6b0ae5ed1a6e5c
surazgaundel <gaundelsuraz99@gmail.com>
2023-07-09 17:07:01 +0530

    Second Commit

--- src/List.jsx
+++ src/List.jsx
@@ -6,10 +6,12 @@ export default function List({list,handleRemove,handleEdit}) {
   return (
     <div>
       {list.map(({id,item})=>
-        (<div key={id} className='flex gap-1'>
+        (<div key={id} className='flex gap-5 justify-between'>
           <p className='m-1'>{item}</p>
+          <section>
           <button type='button' onClick={()=>handleEdit(id)}>🖊</button>
           <button type='button' onClick={()=>handleRemove(id)}>💥</button>
+          </section>
         </div>)
       )}
     </div>
